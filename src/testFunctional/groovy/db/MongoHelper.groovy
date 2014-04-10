d84e0c3fbba17e874f711c48b9160530bf205bfa
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-04-10 21:04:36 +0100

    Introduces MongoHelper for DB interactions.

---
+++ src/testFunctional/groovy/db/MongoHelper.groovy
@@ -0,0 +1,34 @@
+package db
+
+import com.mongodb.BasicDBObjectBuilder
+import com.mongodb.DB
+import com.mongodb.MongoClient
+import com.mongodb.WriteConcern
+
+import java.util.concurrent.atomic.AtomicLong
+
+import static com.mongodb.BasicDBObjectBuilder.start
+
+class MongoHelper {
+
+    static id = new AtomicLong()
+
+    static prepareDB(){
+        def mongo = new MongoClient()
+        mongo.writeConcern = WriteConcern.NORMAL
+        mongo.getDB("gvm")
+    }
+
+    static insertBroadcastInDb(DB db, String broadcast){
+        def collection  = db.getCollection("broadcast")
+        BasicDBObjectBuilder builder = start()
+            .add("_id", id.getAndIncrement().toString())
+            .add("text", broadcast)
+        collection.insert(builder.get())
+    }
+
+    static dropCollectionFromDb(DB db, String collection){
+        def dbCollection  = db.getCollection(collection)
+        dbCollection.drop()
+    }
+}
