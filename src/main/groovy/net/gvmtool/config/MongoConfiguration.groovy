a94290ef91f0614dab102df9667699130845eb72
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-07-13 14:49:08 +0100

    Makes mongodb configurable through environment variables: * MONGO_HOST * MONGO_PORT * MONGO_DB_NAME

--- src/main/groovy/net/gvmtool/config/MongoConfiguration.groovy
+++ src/main/groovy/net/gvmtool/config/MongoConfiguration.groovy
@@ -1,18 +1,30 @@
 package net.gvmtool.config
 
 import com.mongodb.Mongo
+import com.mongodb.MongoClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 
 @Configuration
 class MongoConfiguration extends AbstractMongoConfiguration {
+
+    @Value("#{systemEnvironment['MONGO_HOST']}")
+    String mongoHost = "localhost"
+
+    @Value("#{systemEnvironment['MONGO_PORT']}")
+    String mongoPort = "27017"
+
+    @Value("#{systemEnvironment['MONGO_DB_NAME']}")
+    String mongoDbName = "gvm"
+
     @Override
     protected String getDatabaseName() {
-        "gvm"
+        mongoDbName
     }
 
     @Override
     Mongo mongo() throws Exception {
-        new Mongo()
+        new MongoClient(mongoHost, mongoPort.toInteger())
     }
 }
