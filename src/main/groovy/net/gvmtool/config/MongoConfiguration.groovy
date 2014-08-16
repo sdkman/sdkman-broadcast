ef7d75e57849a0b41e33e4012de5f57e612444de
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-08-16 19:10:33 +0100

    Excludes class name when persisting.

--- src/main/groovy/net/gvmtool/config/MongoConfiguration.groovy
+++ src/main/groovy/net/gvmtool/config/MongoConfiguration.groovy
@@ -5,8 +5,13 @@ import com.mongodb.MongoClient
 import com.mongodb.MongoCredential
 import com.mongodb.ServerAddress
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 
 @Configuration
 class MongoConfiguration extends AbstractMongoConfiguration {
@@ -34,11 +39,19 @@ class MongoConfiguration extends AbstractMongoConfiguration {
     @Override
     Mongo mongo() throws Exception {
         def serverAddress = new ServerAddress(mongoHost, mongoPort.toInteger())
-        if(mongoUsername && mongoPassword){
+        if (mongoUsername && mongoPassword) {
             def credential = MongoCredential.createMongoCRCredential(mongoUsername, mongoDbName, mongoPassword.toCharArray())
             new MongoClient(serverAddress, [credential])
         } else {
             new MongoClient(serverAddress)
         }
+    }
+
+    @Bean
+    MongoTemplate mongoTemplate() throws Exception {
+        //remove _class
+        def converter = new MappingMongoConverter(mongoDbFactory(), new MongoMappingContext())
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null))
+        new MongoTemplate(mongoDbFactory(), converter)
     }
 }
