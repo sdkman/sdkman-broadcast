fd97efea9668e91af0f0b4dd3f689e369120a6c8
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-08-20 08:35:02 +0100

    Reworks broadcast.js

--- src/testFunctional/js/broadcast.js
+++ src/testFunctional/js/broadcast.js
@@ -1,4 +1,4 @@
-{ "_id" : "0", "text" : "Groovy 2.3.0 final has been released!", "date" : { "$date" : "2014-05-12T00:00:00.000+0200" } }
-{ "_id" : "1", "text" : "Groovy 2.3.0-rc-1 has been released.", "date" : { "$date" : "2014-05-11T00:00:00.000+0200" } }
-{ "_id" : "2", "text" : "Groovy 2.3.0-beta-4 has been released.", "date" : { "$date" : "2014-05-10T00:00:00.000+0200" } }
-{ "_id" : "3", "text" : "Groovy 2.3.0-beta-3 has been released.", "date" : { "$date" : "2014-05-09T00:00:00.000+0200" } }
+db.broadcast.insert({ "_id" : "0", "text" : "Groovy 2.3.0 final has been released!",  "date" : new ISODate("2014-05-12T00:00:00.000+0200") });
+db.broadcast.insert({ "_id" : "1", "text" : "Groovy 2.3.0-rc-1 has been released.",   "date" : new ISODate("2014-05-11T00:00:00.000+0200") });
+db.broadcast.insert({ "_id" : "2", "text" : "Groovy 2.3.0-beta-4 has been released.", "date" : new ISODate("2014-05-10T00:00:00.000+0200") });
+db.broadcast.insert({ "_id" : "3", "text" : "Groovy 2.3.0-beta-3 has been released.", "date" : new ISODate("2014-05-09T00:00:00.000+0200") });
