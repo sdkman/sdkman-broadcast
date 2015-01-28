ea315ddcb4b057b9ff7fe0545a6fd00240d59ac3
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-01-28 08:36:13 +0000

    Minor code style tweak on twitter service.

--- src/main/groovy/net/gvmtool/service/TwitterService.groovy
+++ src/main/groovy/net/gvmtool/service/TwitterService.groovy
@@ -15,10 +15,11 @@
  */
 package net.gvmtool.service
 
-import groovyx.gpars.GParsPool
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.social.twitter.api.impl.TwitterTemplate
 import org.springframework.stereotype.Service
+
+import static groovyx.gpars.GParsPool.withPool
 
 @Service
 class TwitterService {
@@ -32,6 +33,6 @@ class TwitterService {
 
     def tweet = { status -> twitter.timelineOperations().updateStatus(status) }
 
-    def tweetAsync = { status -> GParsPool.withPool() { tweet.async()(status) } }
+    def tweetAsync = { status -> withPool() { tweet.async()(status) } }
 
 }
